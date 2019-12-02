17cca5e9ea2c72f1de6ca697969c11f377e04622
nandoacoelho <nandoacoelho@gmail.com>
2019-12-02 22:54:26 +0100

    Update index to use sticky css property

--- react/index.tsx
+++ react/index.tsx
@@ -1,8 +1,6 @@
-import React, { Fragment, FC, useEffect } from 'react'
-import { path } from 'ramda'
+import React, { Fragment, FC } from 'react'
 import { useCssHandles } from 'vtex.css-handles'
-import { useStickyScroll } from './useStickyScroll'
-
+import styles from './styles.css'
 const CSS_HANDLES = ['container'] as const
 
 enum Positions {
@@ -21,29 +19,6 @@ interface StorefrontComponent extends FC<Props> {
 
 const StickyLayoutComponent: StorefrontComponent = ({ children, position, verticalSpacing = 0 }) => {
   const handles = useCssHandles(CSS_HANDLES)
-  const {
-    containerReference,
-    container,
-    handlePosition,
-    debouncedHandlePosition
-  } = useStickyScroll({ position, verticalSpacing })
-
-  useEffect(() => {
-    window && window.addEventListener('scroll', debouncedHandlePosition)
-    window && window.addEventListener('resize', handlePosition)
-    return () => {
-      window && window.removeEventListener('scroll', debouncedHandlePosition)
-      window && window.removeEventListener('resize', handlePosition)
-    }
-  }, [handlePosition])
-
-  const viewOffset = path<number>(['current', 'offsetTop'], containerReference)
-  // This effect places the button at the right position while the screen is mounting
-  useEffect(() => {
-    if (viewOffset != null) {
-      handlePosition()
-    }
-  }, [handlePosition, viewOffset])
 
   if (!position || (position !== Positions.BOTTOM && position !== Positions.TOP)) {
     return <Fragment>{children}</Fragment>
@@ -51,13 +26,11 @@ const StickyLayoutComponent: StorefrontComponent = ({ children, position, vertic
 
   return (
     <Fragment>
-      <div ref={containerReference}></div>
       <div
-        ref={container}
-        className={handles.container}
+        className={`${handles.container} ${styles.sticky}`}
         style={{
-          position: 'relative',
-          bottom: 0,
+          ...(position === Positions.TOP ? { top: verticalSpacing } : {}),
+          ...(position === Positions.BOTTOM ? { bottom: verticalSpacing } : {}),
           zIndex: 10,
         }}
       >
