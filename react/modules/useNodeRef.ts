bfcf9d84e20611dee0c379779d9c841c6659928c
Christian Kaisermann <christian@kaisermann.me>
2020-02-04 14:42:17 -0300

    Delete unused module

--- react/modules/useNodeRef.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-type RefFn = <T>(instance: T | null) => void
-
-export const useNodeRef = <T extends HTMLElement>(): [T | null, RefFn] => {
-  const [node, ref] = useState<T | null>(null)
-  return [node, ref as RefFn]
-}
